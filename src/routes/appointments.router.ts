0debbacaf56051c1a7d089db15167eeeb02871a0
Melqui <43079783+melquisedeque-magalhaes@users.noreply.github.com>
2020-10-27 00:25:43 -0300

    gobarber-start-backend

---
+++ src/routes/appointments.router.ts
@@ -0,0 +1,30 @@
+import { Router } from 'express';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
+
+const appointmentsRouter = Router();
+
+const appointments: Appointment[] = [];
+
+appointmentsRouter.post('/', (request, response) => {
+    const { provider, date } = request.body;
+
+    const parseDate = startOfHour(parseISO(date));
+
+    const findAppointmentInSameDate = appointments.find(appointment =>
+        isEqual(parseDate, appointment.date),
+    );
+
+    if (findAppointmentInSameDate)
+        return response
+            .status(400)
+            .json({ msg: 'This appointment is already booked' });
+
+    const appointment = new Appointment(provider, parseDate);
+
+    appointments.push(appointment);
+
+    return response.json(appointment);
+});
+
+export default appointmentsRouter;
