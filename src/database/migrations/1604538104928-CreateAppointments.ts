7b94b1c12bc867f4b5e2e6ca9b57aa7c552eadb2
Melqui <43079783+melquisedeque-magalhaes@users.noreply.github.com>
2020-11-05 00:22:15 -0300

    first migration

---
+++ src/database/migrations/1604538104928-CreateAppointments.ts
@@ -0,0 +1,31 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export default class CreateAppointments1604538104928 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'appointments',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'varchar',
+                    isPrimary: true,
+                    generationStrategy: 'uuid',
+                },
+                {
+                    name: 'provider',
+                    type: 'varchar'
+                },
+                {
+                    name: 'date',
+                    type: 'timestamp with time zone'
+                }
+            ]
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('appointments')
+    }
+
+}
